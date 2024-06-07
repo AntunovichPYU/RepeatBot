d01d9fec4a646efd4e6716eb79b965895c233358
Antunovich Pavel <pasha.antunovich@gmail.com>
2024-06-07 18:45:20 +0300

    initial commit

---
+++ src/main/kotlin/ru/antunovichpyu/RepeatBot/RepeatBotApplication.kt
@@ -0,0 +1,11 @@
+package ru.antunovichpyu.RepeatBot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RepeatBotApplication
+
+fun main(args: Array<String>) {
+	runApplication<RepeatBotApplication>(*args)
+}
